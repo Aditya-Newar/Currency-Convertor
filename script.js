7389b8a1f7bc38742c06b86b265ce6e86a887d41
The Beast <pradhanaditya555@gmail.com>
2024-12-30 09:43:18 +0530

    adding options

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 let url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json";
+
+const dropDowns = document.querySelectorAll(".dropdown select");
+
+for (let select of dropDowns){
+    for (currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        select.append(newOption);
+    }
+}
